ebdfa27aebcc9bd99c386e1ef28a613fc5f9ec33
AceEagleGamer <justinkenrickgg@gmail.com>
2018-08-18 21:24:20 +0800

    cookie clicker update
    wow new cookie clicker XDyayay

---
+++ Projects/CookieClicker/cookie.js
@@ -0,0 +1,49 @@
+var cookiecount = 0;
+
+// upgrades
+
+var autoClickers = 0;
+
+window.onload = function() {
+  load();
+}
+
+function update() {
+  document.getElementById('text').value = cookiecount;
+  document.title = cookiecount + " Cookies";
+  document.getElementById('amountAC').innerHTML = "You own " + autoClickers + " Auto Clickers";
+  document.getElementById('costAC').innerHTML = ((autoClickers+1) * 12) + " Cookies";
+}
+function timer() {
+  cookiecount += autoClickers;
+  update();
+}
+setInterval(timer,1000)
+
+function buyAutoClicker() {
+  if (cookiecount >= ((autoClickers+1) * 12)) {
+    cookiecount = cookiecount - ((autoClickers+1) * 12);
+    autoClickers += 1;
+    update();
+  }
+}
+
+function addCookie() {
+  cookiecount += 1;
+  console.log(cookiecount);
+
+  update();
+}
+
+function save() {
+  localStorage.setItem("cookiecount",cookiecount);
+  localStorage.setItem("amountAC", autoClickers);
+}
+
+function load() {
+  cookiecount = localStorage.getItem("cookiecount");
+  cookiecount = parseInt(cookiecount);
+  autoClickers = localStorage.getItem("amountAC");
+  autoClickers = parseInt(autoClickers);
+  update();
+}
